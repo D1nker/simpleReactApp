ebab6d6974c6e504cb88d89ffe333b8556a63c72
Quentin F <q.faure@creads.org>
2020-03-21 12:41:14 +0100

    minor css update

--- src/components/Todo/TodoItem.jsx
+++ src/components/Todo/TodoItem.jsx
@@ -6,15 +6,15 @@ const TodoItem = () => {
 
   return (
     <div className="App-todoItem">
-    <ListGroup as="ul">
-      <ListGroup.Item as="li" active>
+    <ListGroup >
+      <ListGroup.Item action href="#link1">
        Todo 1
       </ListGroup.Item>
-      <ListGroup.Item as="li">Todo 2</ListGroup.Item>
-      <ListGroup.Item as="li" disabled>
+      <ListGroup.Item action href="#link2">Todo 2</ListGroup.Item>
+      <ListGroup.Item disabled action href="#link3">
         Todo 3
       </ListGroup.Item>
-      <ListGroup.Item as="li">Todo 4</ListGroup.Item>
+      <ListGroup.Item action href="#link4">Todo 4</ListGroup.Item>
     </ListGroup>
     </div>
   );
