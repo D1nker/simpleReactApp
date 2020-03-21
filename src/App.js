d4738d82f54d3b5ead3599e65735dc1b9cf0602a
Quentin F <q.faure@creads.org>
2020-03-21 17:30:42 +0100

    add props / json server

--- src/App.js
+++ src/App.js
@@ -1,16 +1,26 @@
 import React from 'react';
 import './assets/css/App.css';
-import MainContent from './components/MainContent';
-import Todos from './components/Todos';
 import Header from './components/Header';
 import Footer from './components/Footer';
+import MainContent from './components/MainContent';
+import Todos from './components/Todos';
+import Card from './components/Card';
 
 const App = () => {
+
+  const contact = {
+    name: "d1nker",
+    imgUrl: "http://placekitten.com/300/200",
+    phone: "(212) 1337-1337",
+    email: "d1nker@d1nker.com",
+  };
+
   return (
     <div className="App">
       <Header />
       <MainContent />
       <Todos />
+      <Card contact={contact} />
       <Footer />
     </div>
   );
--- src/components/Todo/TodoItem.jsx
+++ src/components/Todo/TodoItem.jsx
@@ -7,13 +7,9 @@ const TodoItem = () => {
   return (
     <div className="App-todoItem">
     <ListGroup >
-      <ListGroup.Item action href="#link1">
-       Todo 1
-      </ListGroup.Item>
+      <ListGroup.Item action href="#link1">Todo 1</ListGroup.Item>
       <ListGroup.Item action href="#link2">Todo 2</ListGroup.Item>
-      <ListGroup.Item disabled action href="#link3">
-        Todo 3
-      </ListGroup.Item>
+      <ListGroup.Item disabled action href="#link3">Todo 3</ListGroup.Item>
       <ListGroup.Item action href="#link4">Todo 4</ListGroup.Item>
     </ListGroup>
     </div>
