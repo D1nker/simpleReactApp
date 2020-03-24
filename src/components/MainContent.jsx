b3d5be4da29886a01513d5d5608fa95201ead7a1
Quentin F <q.faure@creads.org>
2020-03-24 19:59:14 +0100

    useRef MainContent component

--- src/components/MainContent.jsx
+++ src/components/MainContent.jsx
@@ -1,26 +1,53 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import '../assets/css/App.css';
 import Button from 'react-bootstrap/Button';
 
-const MainContent = () => {
+const MainContent = ({ initialCount }) => {
+  initialCount = 0;
   // Déclaration d'une nouvelle variable d'état, que l'on appellera “count”
-  const [count, setCount] = useState(0);
+  const [count, setCount] = useState(initialCount);
+  const prevCountRef = useRef();
   // Similaire à componentDidMount et componentDidUpdate :
   useEffect(() => {
+    prevCountRef.current = count;
     document.title = `Vous avez cliqué ${count} fois`;
   });
+
+  const prevCount = prevCountRef.current;
+  console.log(prevCount);
 
   return (
     <div className="App-main-content">
       <main>
         <p>You clicked {count} times</p>
-        <Button
-          variant="primary"
-          size="lg"
-          onClick={() => setCount(count + 1)}
-        >
-          Click here
-        </Button>
+
+        <div>
+          <Button
+            variant="primary"
+            size="lg"
+            onClick={() => setCount(prevCount => prevCount + 1)}
+          >
+            +
+          </Button>
+
+          <Button
+            variant="primary"
+            size="lg"
+            onClick={() => setCount(prevCount => prevCount - 1)}
+          >
+            -
+          </Button>
+
+          <Button
+            variant="primary"
+            size="lg"
+            onClick={() => setCount(initialCount)}
+          >
+            Reset
+          </Button>
+        </div>
+
+
       </main>
     </div>
   );
